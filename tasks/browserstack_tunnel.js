0d0e677e69d85877e71bb17c75794acf07bcd3f7
nadavsinai <ns@nadavsinai.com>
2016-01-29 21:50:56 +0200

    reset indent to 2 spaces

--- tasks/browserstack_tunnel.js
+++ tasks/browserstack_tunnel.js
@@ -1,39 +1,39 @@
 'use strict';
-module.exports = function (grunt) {
-    grunt.registerMultiTask('browserstackTunnel', 'Create BrowserStack tunnel', function () {
-        var options = this.options({
-            accessKey: '',
-            hostname: 'localhost',
-            port: 3000,
-            sslFlag: 0
-        });
-
-        var BrowserStackTunnel = require('browserstacktunnel-wrapper');
-        var browserStackTunnel = new BrowserStackTunnel({
-            key: options.accessKey,
-            hosts: [{
-                name: options.hostname,
-                port: options.port,
-                sslFlag: options.sslFlag
-            }]
-        });
-        var done = this.async();
-        var cleanup = function () {
-            console.log('stopping tunnel');
-            browserStackTunnel.stop(done);
-        };
-        process.stdin.resume();
-        browserStackTunnel.start(function (error) {
-            if (error) {
-                grunt.log.error('Could not start tunnel');
-                grunt.log.debug(error);
-                done(false);
-            } else {
-                grunt.log.ok('Start tunnel successfully');
-                process.on('uncaughtException', cleanup);
-                process.on('exit', cleanup);
-                process.on('SIGINT', cleanup);
-            }
-        });
+module.exports = function(grunt) {
+  grunt.registerMultiTask('browserstackTunnel', 'Create BrowserStack tunnel', function() {
+    var options = this.options({
+      accessKey: '',
+      hostname: 'localhost',
+      port: 3000,
+      sslFlag: 0
     });
+
+    var BrowserStackTunnel = require('browserstacktunnel-wrapper');
+    var browserStackTunnel = new BrowserStackTunnel({
+      key: options.accessKey,
+      hosts: [{
+        name: options.hostname,
+        port: options.port,
+        sslFlag: options.sslFlag
+      }]
+    });
+
+    var done = this.async();
+    var cleanup = function () {
+      grunt.log.ok('stopping tunnel');
+      browserStackTunnel.stop(done);
+    };
+    process.stdin.resume();
+    browserStackTunnel.start(function (error) {
+      if (error) {
+        grunt.log.error('Could not start tunnel');
+        grunt.log.debug(error);
+        done(false);
+      } else {
+        grunt.log.ok('Start tunnel successfully');
+        process.on('uncaughtException', cleanup);
+        process.on('exit', cleanup);
+        process.on('SIGINT', cleanup);
+    }});
+  });
 };
